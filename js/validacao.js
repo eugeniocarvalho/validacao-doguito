ece423ddbcc400899dfb4c9d21fc154c98af3c97
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-07-09 11:30:05 -0300

    formating and checking cpf

--- js/validacao.js
+++ js/validacao.js
@@ -1,5 +1,6 @@
 const validadores = {
-  dataNascimento:input => validaDataNascimento(input)
+  dataNascimento:input => validaDataNascimento(input),
+  cpf: input => validaCPF(input)
 };
 
 const mensagensDeErro = {
@@ -17,6 +18,10 @@ const mensagensDeErro = {
   dataNascimento: {
     valueMissing: 'O campo data está vazio',
     customError: 'Idade deve ser maior de 18 anos.'
+  },
+  cpf: {
+    valueMissing: 'O campo cpf está vazio',
+    customError: 'CPF não válido'
   }
 }
 
@@ -66,4 +71,67 @@ function maiorQue18(dataInput) {
   const dataMais18 = new Date(dataInput.getUTCFullYear() + 18, dataInput.getUTCMonth(), dataInput.getUTCDate());
 
   return dataMais18 <= dataAtual;
+}
+
+function validaCPF(input) {
+  const cpfFormatado = input.value.replace('\D/g', "");
+  let mensagem = '';
+
+  if (!checaCPFRepetido(cpfFormatado) || !checaestruturaCPF(cpfFormatado))
+    mensagem = "O CPF não é válido";
+
+  input.setCustomValidity(mensagem);
+}
+
+function checaCPFRepetido(cpf) {
+  const valoresRepetidos = [
+    '11111111111', 
+    '22222222222', 
+    '33333333333', 
+    '44444444444', 
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999'
+  ];
+
+  let cpfValido = true;
+
+  valoresRepetidos.forEach(valor => {
+    if (valor === cpf)
+      cpfValido = false;
+  });
+
+  return cpfValido;
+}
+
+function checaestruturaCPF(cpf) {
+  const multiplicador = 10;
+
+  return checaDigitoVerificador(cpf, multiplicador);
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+  if (multiplicador >= 12)
+    return true;
+
+  let multiplicadorInicial = multiplicador;
+  let soma = 0;
+  const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+  const digitoVerificador = cpf.charAt(multiplicador - 1);
+
+  for (let i = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+    soma = soma + cpfSemDigitos[i] * multiplicadorInicial;
+    i++;
+  }
+
+  if (digitoVerificador == confirmaDigito(soma))
+    return checaDigitoVerificador(cpf, multiplicador + 1);
+  
+  return false;
+}
+
+function confirmaDigito(soma) {
+  return 11 - (soma % 11);
 }
